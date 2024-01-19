ca86fabacedb5aaec8223cce90942a6c24d27998
Christian Kearns <christian.kearns@ufl.edu>
2024-01-19 18:26:04 -0500

    mysql to nextjs pipeline functional. displays blog posts from db

---
+++ src/app/api/getposts/route.tsx
@@ -0,0 +1,24 @@
+import { NextResponse } from "next/server";
+import executeQuery from "../../lib/db";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+    
+    try  {
+        const result = await executeQuery({
+            query: 'SELECT * FROM posts',
+            values: '',
+        })
+        console.log(result);
+
+        if (res) {
+            return NextResponse.json({posts: result}, {status: 200})
+        } else {
+            console.error('Response object is undefined.');
+        }
+
+    } catch ( error ) {
+        console.log( error );
+    }
+}
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,11 +1,73 @@
-import React from 'react'
-import Nav from '../components/nav'
+'use client'
+import React, { useEffect, useState } from 'react';
+import Nav from '../components/nav';
+import styles from './page.module.css';
+
+type Post = {
+  post_id: number,
+  author_id: number,
+  post: string,
+  author: string,
+  created_at: Date,
+  last_modified: Date
+}
 
 export default function BlogPage() {
+  const [posts, setPosts] = useState<Post[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await fetch("/api/getposts");
+
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+
+        const data = await res.json();
+        setPosts(data.posts);
+      } catch (error) {
+        console.error(error);
+        setError('Failed to load data');
+      }
+    }
+
+    getData();
+  }, []);
+
+  useEffect(() => {
+    if(posts) {
+      setLoading(false);
+      console.log(posts[0].author)
+    }
+  }, [posts]);
+
   return (
     <>
       <Nav />
-      <div>BlogPage</div>
+      <div>
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p>Error: {error}</p>
+        ) : posts && posts.length > 0 ? (
+          <div className={styles.postsContainer} key={1}>
+            <p className={styles.title} key={2}>Blog</p>
+            {posts.map(post => (
+              <div className={styles.postContainer} key={post.post_id}>
+                <p className={styles.author} key={post.author_id}>{post.author}</p>
+                <p className={styles.post} key={post.post_id}>{post.post}</p>
+                <p key={post.post_id}>Created at: {new Date(post.created_at).toLocaleString()}</p>
+                <p key={post.post_id}>Last Modified: {new Date(post.last_modified).toLocaleString()}</p>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>No posts available.</p>
+        )}
+      </div>
     </>
-  )
+  );
 }
