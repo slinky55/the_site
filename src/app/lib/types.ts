207bef1d0f1f961b9e3961cc5e70165c84519179
slinky55 <larry.mason@ufl.edu>
2024-02-20 00:27:30 -0500

    added signup/signin with email + db functionality

---
+++ src/app/lib/types.ts
@@ -0,0 +1,11 @@
+type User = {
+    id: string;
+    name: string;
+    email: string;
+    emailVerified: boolean;
+    image: string;
+    passwordHash: string;
+    privilegeLevel: string;
+    createdAt: string;
+    updatedAt: string;
+}
