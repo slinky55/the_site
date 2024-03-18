334f8f5a9179ffddaa6b9358a41f0c1041cc3bde
Christian Kearns <christian.kearns@ufl.edu>
2024-03-18 12:28:22 -0400

    fixed contact page to represent luis's contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,8 +9,8 @@ import Link from 'next/link';
 
 
 export default function ContactPage() {
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
   const [subject, setSubject] = useState('');
   const [msg, setMsg] = useState('');
@@ -22,104 +22,109 @@ export default function ContactPage() {
             "Content-Type": "application/json",
             },
             body: JSON.stringify({
-              inquiry_id: uuidv4(),
-              user_id: uuidv4(), // Larry please fix this to be the id of the currently signed in user
-              subject: subject,
+              msg_id: uuidv4(),
+              author_id: uuidv4(),
+              msg: msg,
+              author: name,
               email: email,
-              content: msg
+              msg_subject: subject,
             })
         }
 
         await fetch('/api/inquiries/createinquiry', postData);
     }
 
-  return (
-    <>
-      <Header />
-      <div className={styles.container}>
-        <h1 className={styles.title}>Contact Us</h1>
-        <div className={styles.instruction}>Use the form below to directly contact a group administrator. We'll respond as soon as we can!</div>
-        <div className={styles.contactBox}>
-          <div className={styles.contactLeft}>
-            <form>
-              <div className={styles.inputRow}>
-                <div className={styles.inputGroup}>
-                  <label className={styles.label}>Name</label>
-                  <input 
-                  className={styles.input} 
-                  type="text" 
-                  id="name"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  placeholder='John Doe'
-                  required />
+    return (
+      <>
+        <Header />
+        <div className={styles.container}>
+          <div className={styles.contactBox}>
+            <div className={styles.contactLeft}>
+            <center><h1 className={styles.title}>Contact Us</h1></center>
+            <div className={styles.instruction}>Use the form below to directly contact a group administrator. We'll respond as soon as we can!</div>
+              <form>
+                <div className={styles.inputRow}>
+                  <div className={styles.inputGroup}>
+                    <label className={styles.label}>First Name</label>
+                    <input 
+                    className={styles.input} 
+                    type="text" 
+                    id="firstName"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
+                    placeholder='John'
+                    required />
+                  </div>
+
+                  <div className={styles.inputGroup}>
+                    <label className={styles.label}>Last Name</label>
+                    <input 
+                    className={styles.input} 
+                    type="text" 
+                    id="name"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
+                    placeholder='Doe'
+                    required />
+                  </div>
                 </div>
 
-                <div className={styles.inputGroup}>
-                  <label className={styles.label}>Phone Number</label>
-                  <input 
-                  className={styles.input} 
-                  type="text" 
-                  id="phoneNumber"
-                  value={phoneNumber}
-                  onChange={(e) => setPhoneNumber(e.target.value)}
-                  placeholder='+1 XXX-XXX-XXXX'
-                  required />
-                </div>
+                <div className={styles.inputRow}>
+                  <div className={styles.inputGroup}>
+                    <label className={styles.label}>Email</label>
+                    <input 
+                    className={styles.input} 
+                    type="email" 
+                    id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder='************@ufl.edu'
+                    required />
+                  </div>
 
+                  <div className={styles.inputGroup}>
+                    <label className={styles.label}>Subject</label>
+                    <input 
+                    className={styles.input} 
+                    type="text"
+                    id="subject"
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                    required />
+                  </div>
+  
+                </div>
+  
+                <label className={styles.label}>Message</label>
+                <textarea 
+                maxLength={250}
+                className={styles.messageTextArea}
+                id="msg"
+                value={msg}
+                onChange={(e) => setMsg(e.target.value)}
+                placeholder='Max number of characters: 250'
+                required></textarea>
+                <center><button className={styles.submitButton} onClick={createInquiry}>Submit</button> </center>
+              </form>
+            </div>
+            <div className={styles.contactRight}>
+              <div className={styles.contactRightBody}>Follow us on social media to keep in touch with the Technology, Health, and Equity Workgroup's latest updates.</div>
+              <div className={styles.socialsLinks}>
+                <hr className={styles.customHR} />
+                <Link href="instagram.com">
+                  <FontAwesomeIcon className={styles.socialsLink} icon={faInstagram}/>
+                </Link>
+                <Link href="facebook.com">
+                  <FontAwesomeIcon className={styles.socialsLink} icon={faFacebook} />
+                </Link>
+                <Link href="twitter.com">
+                  <FontAwesomeIcon className={styles.socialsLink} icon={faTwitter} />
+                </Link>
+                <hr className={styles.customHR} />
               </div>
-              <div className={styles.inputRow}>
-                <div className={styles.inputGroup}>
-                  <label className={styles.label}>Email</label>
-                  <input 
-                  className={styles.input} 
-                  type="email" 
-                  id="email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                  placeholder='************@ufl.edu'
-                  required />
-                </div>
-
-                <div className={styles.inputGroup}>
-                  <label className={styles.label}>Subject</label>
-                  <input 
-                  className={styles.input} 
-                  type="text"
-                  id="subject"
-                  value={subject}
-                  onChange={(e) => setSubject(e.target.value)}
-                  required />
-                </div>
-
-              </div>
-
-              <label className={styles.label}>Message</label>
-              <textarea 
-              className={styles.messageTextArea}
-              id="msg"
-              value={msg}
-              onChange={(e) => setMsg(e.target.value)}
-              required></textarea>
-              <button onClick={createInquiry}>Submit</button>
-            </form>
-          </div>
-          <div className={styles.contactRight}>
-            <div className={styles.contactRightBody}>Follow us on social media to keep in touch with the Technology, Health, and Equity Workgroup's latest updates.</div>
-            <div className={styles.socialsLinks}>
-              <Link href="instagram.com">
-                <FontAwesomeIcon className={styles.socialsLink} icon={faInstagram}/>
-              </Link>
-              <Link href="facebook.com">
-                <FontAwesomeIcon className={styles.socialsLink} icon={faFacebook} />
-              </Link>
-              <Link href="twitter.com">
-                <FontAwesomeIcon className={styles.socialsLink} icon={faTwitter} />
-              </Link>
             </div>
           </div>
         </div>
-      </div>
-    </>
-  )
-}
+      </>
+    )
+  }
