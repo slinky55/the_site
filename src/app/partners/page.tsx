a0b2aed5836a148c4b6bf90ed39e47f195868d8d
alexandrespallone <alexandrespallone@gmail.com>
2024-04-02 21:35:33 -0400

    Backend features added.

--- src/app/partners/page.tsx
+++ src/app/partners/page.tsx
@@ -1,8 +1,39 @@
-import React from 'react';
+'use client'
 import styles from './page.module.css';
 import Image from 'next/image';
+import { Partner } from '@/app/types/partner';
+import React, { useEffect, useState } from 'react';
 
 export default function PartnersPage() {
+  const [partners, setPartners] = useState<Partner[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getData() {
+        try {
+            const res = await fetch("/api/partners/getpartners");
+
+            if (!res.ok) {
+                throw new Error(`HTTP error! Status: ${res.status}`);
+            }
+
+            const data = await res.json();
+
+            if (!Array.isArray(data.partners)) {
+                throw new Error('Unexpected data format');
+            }
+
+            setPartners(data.partners);
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    getData();
+}, []);
+
   return (
     <>
       <div id={styles.toolbar}>
@@ -15,41 +46,34 @@ export default function PartnersPage() {
           <a href="#link3" className={styles.navLink}>Projects</a>
           <a href="#link3" className={styles.navLink}>Blog</a>
           <a href="#link3" className={styles.navLink}>Contact Us</a>
+        </div>
+        <div>
+          <a href="#link9" className={`${styles.signInButton}`}>Sign In</a>
+        </div>
+      </div>
+      <div id={styles.redgraientsection}>
+        <span style={{ width: "100vw" }}>Our Partners</span>
+        <div id={styles.centerDisplayText}>
+          <span>Introducing our partners who share our commitment to promoting health equity through technology and community engagement. Together, we strive to address disparities and empower underserved communities for a healthier tomorrow. Feel free to check them out below.</span>
+        </div>
+      </div>
+      <div id={styles.partnerblock}>
+        {loading ? (
+          <span>Loading...</span>
+        ) : partners.map((partner, index) => (
+          <div id={styles.threeboxes} key={index}>
+            <Image src={partner.logo} width={300} height={200} alt="Image" />
+            <span className={styles.paragraphHeader}>{partner.name}</span>
+            <span className={styles.paragraphContents}>{partner.description}</span>
           </div>
-      <div>
-        <a href="#link9" className={`${styles.signInButton}`}>Sign In</a>
+        ))}
       </div>
+      <div id={`${styles.toolbar}`} style={{ justifyContent: 'center', color: 'white' }}>
+        <span className={styles.connectWithUs} style={{ marginRight: '20px' }}>Connect With Us</span>
+        <span className={styles.logo} style={{ marginRight: '20px' }}>Logo</span>
+        <span className={styles.logo} style={{ marginRight: '20px' }}>Logo</span>
+        <span className={styles.logo} style={{ marginRight: '20px' }}>Logo</span>
       </div>
-        <div id={styles.redgraientsection}>
-            <span style={{width: "100vw"}}>Our Partners</span>
-            <div id={styles.centerDisplayText}><span>Introducting our partenrs who share our commitment to promoting health equity through technology and community engagement. Together, we strive to address disparities and empower underserved communities for a healthier tomorrow. Feel free to check them out below.</span></div>
-        </div>
-        <div id={styles.partnerblock}>
-            <div id={styles.threeboxes}>
-                <Image src="/uflogo.png" width={"300"} height={"200"} alt="Image" />
-                <span className={styles.paragraphHeader}>University of Florida</span>
-                <span className={styles.paragraphContents}>The University of Florida is a renowned public research university located in Gainesville, Florida, offering a wide array of undergraduate, graduate, and professional degree programs in diverse fields.</span>
-            </div>
-            <div id={styles.threeboxes}>
-                <Image src="/uflogo.png" width={"300"} height={"200"} alt="Image" />
-                <span className={styles.paragraphHeader}>University of Florida</span>
-                <span className={styles.paragraphContents}>The University of Florida is a renowned public research university located in Gainesville, Florida, offering a wide array of undergraduate, graduate, and professional degree programs in diverse fields.</span>
-            </div>
-            <div id={styles.threeboxes}>
-                <Image src="/uflogo.png" width={"300"} height={"200"} alt="Image" />
-                <span className={styles.paragraphHeader}>University of Florida</span>
-                <span className={styles.paragraphContents}>The University of Florida is a renowned public research university located in Gainesville, Florida, offering a wide array of undergraduate, graduate, and professional degree programs in diverse fields.</span>
-            </div>
-        </div>
-        <div id={`${styles.toolbar}`} style={{ justifyContent: 'center', color: 'white' }}>
-          <span className={styles.connectWithUs} style={{ marginRight: '20px'}}>Connect With Us</span>
-          <span className={styles.logo} style={{ marginRight: '20px'}}>Logo</span>
-          <span className={styles.logo} style={{ marginRight: '20px'}}>Logo</span>
-          <span className={styles.logo} style={{ marginRight: '20px'}}>Logo</span>
-        </div>
     </>
   );
 }
-
-
-
