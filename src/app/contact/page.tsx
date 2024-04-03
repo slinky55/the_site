b99a55248ff57683855bf0836c813d1dd6e73f2d
ed <edlang114@gmail.com>
2024-04-03 16:44:13 -0400

    fixed the createInquiry/recaptcha updated calendar to show events.

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -9,11 +9,15 @@ export default function ContactPage() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
   const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
   const [msg, setMsg] = useState('');
   const [captcha, setCaptcha] = useState(null);
 
   async function createInquiry() {
+    if (!captcha) {
+      alert('Please verify the captcha before submitting the form.');
+      return;
+    }
+
     const postData = {
       method: "POST",
       headers: {
@@ -25,7 +29,7 @@ export default function ContactPage() {
         first_name: firstName,
         last_name: lastName,
         email: email,
-        subj: subject,
+        msg: msg,
       })
     }
     await fetch('/api/inquiries/createinquiry', postData);
@@ -41,7 +45,7 @@ export default function ContactPage() {
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact Us</h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
-            Use the form below to directly contact a group administrator. We'll respond as
+            Use the form below to directly contact a group administrator. We&apos;ll respond as
             soon as we can!
           </p>
         </div>
@@ -99,7 +103,7 @@ export default function ContactPage() {
               </label>
               <div className="mt-2.5">
               <textarea
-                  name="message"
+                  name="msg"
                   rows={4}
                   className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-red-400 sm:text-sm sm:leading-6"
                   defaultValue={''}
@@ -116,7 +120,6 @@ export default function ContactPage() {
                   className={styles.captcha}
                   sitekey="6Lelfp4pAAAAAFxiaaIe0QtZEOxOtk_OJJ77Jujw"
                   onChange={(val: any) => setCaptcha(val)}
-                  required
               />
             </div>
 
@@ -125,6 +128,7 @@ export default function ContactPage() {
             <button
                 type="submit"
                 className="block w-full rounded-md bg-red-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-red-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-400"
+                onClick={createInquiry}
             >
               Send Message
             </button>
