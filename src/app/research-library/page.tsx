fd41a6c8b7e8c25486ef0c28344ed1f5756f37ed
alexandrespallone <alexandrespallone@gmail.com>
2024-04-17 13:08:15 -0400

    Most resizing is done except for iPhone 5 dimension issues.

--- src/app/research-library/page.tsx
+++ src/app/research-library/page.tsx
@@ -1,16 +1,16 @@
-import React from 'react'
+import React from 'react';
 import styles from './page.module.css';
+import Image from 'next/image';
 
 export default function ResearchLibraryPage() {
   return (
-    <>
-      <div id={styles.functionalBody}>
-        <div style={{width: "100vw", height: "20vh", display: "flex", color: "white", backgroundColor: "red", justifyContent: "center", alignItems: "center"}}>
-          <input id={styles.inputSearch}>
-          </input>
+    <div className={styles.container}>
+      <div id="functionalBody">
+        <div id={styles.redBoxArea}>
+          <input id={styles.inputSearch} />
         </div>
 
-        <div style={{display: "flex"}}>
+        <div style={{ display: 'flex' }}>
           <div id={styles.verticalSection}>
             <span className={styles.subsection}>Sort by date ↓</span>
             <span className={styles.subsection}><hr className={styles.hrStyle}></hr></span>
@@ -29,12 +29,57 @@ export default function ResearchLibraryPage() {
             <span className={styles.subsection}>New Discoveries</span>
           </div>
 
-          <div id={styles.horizontalSection}>
-
+          <div id={styles.horizontalSection} className={styles.horizontalSection}>
+            <div id={styles.whiteDisplayArea}>
+              <span className={styles.whiteDisplaySubsection}>
+                <Image src={"/book.png"} width="50" height="50" style={{ marginRight: '35px' }} alt="This is a book icon" />
+                <div className={styles.textContainer}>
+                  <span style={{ color: 'grey', width: '100%' }}><span>Article</span></span>
+                  <span style={{ color: 'red', width: '100%' }}>How AI Can be used in the World of Modern Medicine</span>
+                  <span style={{ width: '100%' }}>Society of AI Medicine</span>
+                  <span style={{ width: '100%' }}>2016</span>
+                </div>
+              </span>
+              <span className={styles.whiteDisplaySubsection}>
+                <Image src={"/book.png"} width="50" height="50" style={{ marginRight: '35px' }} alt="This is a book icon" />
+                <div className={styles.textContainer}>
+                  <span style={{ color: 'grey', width: '100%' }}><span>Article</span></span>
+                  <span style={{ color: 'red', width: '100%' }}>How AI Can be used in the World of Modern Medicine</span>
+                  <span style={{ width: '100%' }}>Society of AI Medicine</span>
+                  <span style={{ width: '100%' }}>2016</span>
+                </div>
+              </span>
+              <span className={styles.whiteDisplaySubsection}>
+                <Image src={"/book.png"} width="50" height="50" style={{ marginRight: '35px' }} alt="This is a book icon" />
+                <div className={styles.textContainer}>
+                  <span style={{ color: 'grey', width: '100%' }}><span>Article</span></span>
+                  <span style={{ color: 'red', width: '100%' }}>How AI Can be used in the World of Modern Medicine</span>
+                  <span style={{ width: '100%' }}>Society of AI Medicine</span>
+                  <span style={{ width: '100%' }}>2016</span>
+                </div>
+              </span>
+              <span className={styles.whiteDisplaySubsection}>
+                <Image src={"/book.png"} width="50" height="50" style={{ marginRight: '35px' }} alt="This is a book icon" />
+                <div className={styles.textContainer}>
+                  <span style={{ color: 'grey', width: '100%' }}><span>Article</span></span>
+                  <span style={{ color: 'red', width: '100%' }}>How AI Can be used in the World of Modern Medicine</span>
+                  <span style={{ width: '100%' }}>Society of AI Medicine</span>
+                  <span style={{ width: '100%' }}>2016</span>
+                </div>
+              </span>
+              <span className={styles.whiteDisplaySubsection}>
+                <Image src={"/book.png"} width="50" height="50" style={{ marginRight: '35px' }} alt="This is a book icon" />
+                <div className={styles.textContainer}>
+                  <span style={{ color: 'grey', width: '100%' }}><span>Article</span></span>
+                  <span style={{ color: 'red', width: '100%' }}>How AI Can be used in the World of Modern Medicine</span>
+                  <span style={{ width: '100%' }}>Society of AI Medicine</span>
+                  <span style={{ width: '100%' }}>2016</span>
+                </div>
+              </span>
+            </div>
           </div>
         </div>
-
       </div>
-    </>
-  )
+    </div>
+  );
 }
