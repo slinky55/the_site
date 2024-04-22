ff80c56a38c55e665d61e0c02bf813d892c30723
Christian Kearns <christian.kearns@ufl.edu>
2024-04-22 14:31:05 -0400

    partners and research library

--- src/app/blog/view.tsx
+++ src/app/blog/view.tsx
@@ -170,7 +170,7 @@ export default function BlogPage() {
                     <p>Error: {error}</p>
                 ) : posts && posts.length > 0 ?
                     (
-                        <div className="bg-white py-24 sm:py-32">
+                        <div className="bg-white py-4 sm:py-8">
                             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                                 <div className="mx-auto max-w-2xl lg:max-w-4xl">
                                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">From the blog</h2>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -45,7 +45,7 @@ export default async function Projects() {
       }
     
     return (
-        <div className="bg-white py-24 sm:py-32">
+        <div className="bg-white py-4 sm:py-8">
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
                     <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Projects</h2>
